1accf783d09a1963b2e947becdf12a9edc5d9bcd
Christianpepe <christian200405corona@gmail.com>
2025-07-31 14:36:24 -0600

    Ver 0.5.13 cambio en urls

--- Vuelos_reservas/config/settings.py
+++ Vuelos_reservas/config/settings.py
@@ -91,8 +91,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+LOGIN_URL = '/ingreso/'
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
 

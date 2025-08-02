c08b66a0e3c43e6a5224f43c293665b03213e258
Christianpepe <christian200405corona@gmail.com>
2025-08-02 16:54:27 -0600

    VER 0.6.20 cambios en settings

--- Vuelos_reservas/apps/vuelos/apps.py
+++ Vuelos_reservas/apps/vuelos/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 #CORONA GARCIA CHRISTIAN JAVIER
 class VuelosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.vuelos'
+    name = 'Vuelos_reservas.apps.vuelos''
 
     def ready(self):
         import apps.vuelos.signals  # Activa los signals al arrancar la app

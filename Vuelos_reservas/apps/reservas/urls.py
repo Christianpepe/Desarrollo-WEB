e729d8a8ad362c44dbea606e2dae5d805be04c77
Christianpepe <christian200405corona@gmail.com>
2025-08-02 20:03:28 -0600

    VER 0.6.28 arreglos de las urls para el despliegue 3

--- Vuelos_reservas/apps/reservas/urls.py
+++ Vuelos_reservas/apps/reservas/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 #CORONA GARCIA CHRISTIAN JAVIER
 urlpatterns = [
-    path('mis-reservas/', __import__('apps.reservas.views').reservas.views.mis_reservas, name='mis_reservas'),
+    path('mis-reservas/', __import__('Vuelos_reservas.apps.reservas.views').reservas.views.mis_reservas, name='mis_reservas'),
 ]

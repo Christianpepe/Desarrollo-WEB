4b488bbef57461945d6bf431765eb21f5b4c33ac
Christianpepe <christian200405corona@gmail.com>
2025-08-02 20:10:05 -0600

    VER 0.6.30 arreglos de las urls para el despliegue 5

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "clave_de_prueba")
 
 DEBUG = False
-ALLOWED_HOSTS = ['tu-nombre.onrender.com']
+ALLOWED_HOSTS = ['desarrollo-web-wvx7.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
